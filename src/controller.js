ec0bf651fa8dd7a033e6cf05e2f554065520e05f
Geremia Pompei <geremiapompei@gmail.com>
2022-11-17 13:53:01 +0100

    new architectural modifications according data structure

--- src/controller.js
+++ src/controller.js
@@ -1,43 +1,37 @@
-import importer from './service/importer.js';
+import ZerynthApi from './service/zerynth_api.js';
 import formatter from './service/formatter.js';
-import exporter from './service/exporter.js';
+import WifeyeApi from './service/wifeye_api.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const wifeye_api = new WifeyeApi(process.env.WIFEYE_API_KEY);
 
 export default {
-    /**
-     * Function to list Zerynth workspaces.
-     * @returns List of Zerynth workspaces.
-     */
-    async workspace_list() {
-        return await importer.workspaces();
-    },
-    /**
-     * Function to list Zerynth devices given a workspace.
-     * @param {String} workspace_id Workspace id to retrieve its devices.
-     * @returns List of devices.
-     */
-    async device_list(workspace_id) {
-        return await importer.devices(workspace_id);
-    },
-    /**
-     * Function to list Zerynth timeseries given a workspace.
-     * @param {Object} config Configuration of timeseries call to filter results.
-     * @returns List of timeseries.
-     */
-    async timeseries_list(config) {
-        return await importer.timeseries(config);
-    },
     /**
      * Function to get timeseries, parse them in fingerprints and send them to wi-feye server.
      * @param {Object} config Configuration of filter for timeseries api call.
      * @returns Wi-feye db service response.
      */
     async fingerprints_transfer(config) {
-        const timeseries = await importer.timeseries(config);
-        if (timeseries.length > 0) {
-            const fingerprints = formatter.parse_timeseries(timeseries);
-            return await exporter.create_fingerprints(fingerprints);
-        } else {
-            throw `not existing detection!`;
+        const users = await wifeye_api.get_users();
+        const fingerprints = [];
+        for(const user of users) {
+            const zerynth_api = new ZerynthApi(user.apik);
+            const fingerprint = {
+                id: user.id,
+                workspaces: []
+            };
+            for(const workspace of user.workspaces) {
+                const timeseries = await zerynth_api.timeseries({...config, workspace_id: workspace.idz});
+                if (timeseries.length > 0) {
+                    fingerprint.workspaces.push({
+                        id: workspace.id,
+                        detections: formatter.parse_timeseries(timeseries)
+                    });
+                }
+            }
+            fingerprints.push(fingerprint);
         }
+        return await wifeye_api.create_fingerprints(fingerprints);
     }
 };
---
+++ src/service/wifeye_api.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+/**
+ * Base url to send storage data of db service.
+ */
+const BASEURL_STORAGE = process.env.WIFEYE_BASEURL_STORAGE;
+
+/**
+ * Wifeye api class that wraps all the api calls to get and send resources to wifeye db service.
+ */
+export default class WifeyeApi {
+
+    /**
+     * Wifeye api constructor.
+     * @param {string} api_key Wifeye api key to authorize rest api calls.
+     */
+    constructor(api_key) {
+        this.api_key = api_key;
+    }
+    /**
+     * Base function to send a post api request to the db service.
+     * @param {String} url Url where send the request.
+     * @param {Object} data Body of the request.
+     * @returns Rest api response of server.
+     */
+    async __post(url, data) {
+        try {
+            const res = await axios.post(url, data, {
+                headers: {
+                    'Authorization': `Bearer ${this.api_key}`,
+                    'Content-type': 'application/json'
+                }
+            });
+            return res.data;
+        } catch (e) {
+            console.error(`Exception of exporter in call ${url}`);
+            return e.data;
+        }
+    }
+    /**
+     * Base function to send a get api request to the db service.
+     * @param {String} url Url where get the resource.
+     * @returns Rest api response of server.
+     */
+    async __get(url) {
+        try {
+            const res = await axios.get(url, {
+                headers: {
+                    'Authorization': `Bearer ${this.api_key}`,
+                }
+            });
+            return res.data;
+        } catch (e) {
+            console.error(`Exception of exporter in call ${url}`);
+            return e.data;
+        }
+    }
+
+    /**
+     * Function able to send the api request to retrieve list of users.
+     * @returns Server api response.
+     */
+    get_users() {
+        return this.__get(`${BASEURL_STORAGE}users`);
+    }
+
+    /**
+     * Function able to send the api request to create a list of fingerprints.
+     * @param {Object[]} fingerprints Fingerprints to send.
+     * @returns Server api response.
+     */
+    create_fingerprints(fingerprints) {
+        return this.__post(`${BASEURL_STORAGE}fingerprints`, fingerprints);
+    }
+}
